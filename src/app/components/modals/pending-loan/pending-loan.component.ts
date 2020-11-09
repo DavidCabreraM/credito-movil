956120d7692c4d68ccd1fcfd1f796e37f36d1cca
david cabrera montes <davyd_d4v1d@hotmail.com>
2020-11-09 02:21:16 -0600

    traduccion de credito pendiente

--- src/app/components/modals/pending-loan/pending-loan.component.ts
+++ src/app/components/modals/pending-loan/pending-loan.component.ts
@@ -8,8 +8,40 @@ import { Component, Input, OnInit } from '@angular/core';
 export class PendingLoanComponent implements OnInit {
   @Input() payment: any;
   constructor() { }
-
+   
+  plazo=""
+  destino=""
   dateCurrent = new Date();
-  ngOnInit() {}
+  ngOnInit() {
+    this.plazo = this.payment.tipo_plazo
+    this.destino = this.payment.proposito_description
+    if(this.payment.proposito_description=="COMPRAR LOCAL O VEHICULO"){
+      this.destino = "BUYLOCAL"
+    }
+    if(this.payment.proposito_description=="ADQUIRIR O COMPRAR MERCANCIA"){
+      this.destino = "ACQUIREMERCHANDISE"
+    }
+    if(this.payment.proposito_description=="COMPRAR MAQUINARIA, EQUIPO O HERRAMIENTAS"){
+      this.destino = "BUYMACHINERY"
+    }
+    if(this.payment.proposito_description=="PAGAR DEUDAS DEL NEGOCIO"){
+      this.destino = "PAYDEBT"
+    }
+    if(this.payment.proposito_description=="OTRO FIN RELACIONADO"){
+      this.destino = "ANOTHEREND"
+    }
+    if(this.payment.proposito_description=="FINES AJENOS AL NEGOCIO"){
+      this.destino = "OUTSIDEBUSINESS"
+    }
 
+    if(this.payment.tipo_plazo=="Semanas"){
+      this.plazo = "Weeks"
+    }
+    if(this.payment.tipo_plazo=="Catorcenas"){
+      this.plazo = "FOURTEEN"
+    }
+    if(this.payment.tipo_plazo=="Meses"){
+      this.plazo = "Months"
+    }
+  }
 }
